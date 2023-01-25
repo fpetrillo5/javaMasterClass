4a766ce7483fe0d1f1e7cc4d5a36f8fef3d0e9c9
Frank Petrillo <frankpetrillo5@gmail.com>
2023-01-25 04:46:10 -0500

    class 47 - methods

---
+++ src/MainChallenge.java
@@ -0,0 +1,125 @@
+public class MainChallenge {
+    public static void main(String[] args){
+
+        //class 44 - if then else challenge
+
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        int finalScore = score;
+
+
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        /*insert a code  segment after the code we have reviewed
+        set the existing score variable to 10000
+        set the existing levelCompleted variable to 8
+        set the existing bonus variable to 200
+        use the same if condition (meaning gameOver is true) you want to
+        perform  the same calculation, and print out the value of the
+        finalScore variable
+
+        wtf is challenging about that
+         */
+
+        //clas 45 - methods in java
+
+        /*a method declares executable code that can be invoked, passing a
+        fixed number of values as arguments
+
+        benefits of the method
+        a method is a way of reducing code duplication
+        a method can be executed many times with potentially different results
+        by passing data to the method in the form of arguments
+
+        structure of the method
+        this method has a name, but takes no data in, and returns no data from the
+        method (which is what the special word void means in the declaration. its accessible
+        to the outside world because it uses the keyword public
+
+        public static void methodName(){
+            //method statements form the method body
+         */
+
+        //class 47 - methods recap
+
+        /* method signature
+        a method is uniquely defined in a class by its name, and the number and
+        type of parameters that are declared for it
+
+        you can have multiple methods with the same method name, as long as
+        the method signature are different
+         */
+
+        //class 48 - method challenge
+
+        int score1 = 25;
+        int score2 = 100;
+        int score3 = 500;
+        int score4 = 1000;
+        int score5= 1500;
+
+        String player1 ="Tim";
+        String player2 ="Paco";
+        String player3 ="2";
+        String player4 ="Prince";
+        String player5 ="Grace";
+
+        int playerRank = calculateHighScorePosition(score1);
+        displayHighScorePosition(player1, playerRank);
+
+         playerRank = calculateHighScorePosition(score2);
+        displayHighScorePosition(player2, playerRank);
+
+         playerRank = calculateHighScorePosition(score3);
+        displayHighScorePosition(player3, playerRank);
+
+         playerRank = calculateHighScorePosition(score4);
+        displayHighScorePosition(player4, playerRank);
+
+         playerRank = calculateHighScorePosition(score5);
+        displayHighScorePosition(player5, playerRank);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+     static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        if(gameOver){
+            int finalScore =score;
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+    }
+
+    public static void displayHighScorePosition(String pName, int pos){
+        System.out.println(pName + " managed to get into position " + pos +
+                " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if(score >= 1000)
+            return 1;
+        else if(score >= 500 && score < 1000)
+            return 2;
+        else if(score >= 100 && score <500)
+            return 3;
+        else
+            return 4;
+    }
+}
--- src/SectionFive.java
+++ src/SectionFive.java
@@ -39,6 +39,23 @@ public class SectionFive {
 
         // class 43 - code blocks and the if then else control statement
 
+        boolean gameOver = true;
+        int score = 400;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(score < 5000 && score > 1000){
+            System.out.println("Your score was less than 5000 but more than 1000");
+        } else if(score < 1000){
+            System.out.println("Your score was less than 1000");
+        } else {
+            System.out.println("You got here");
+        }
+
+        //class 44 - if then else challenge
+
+
+
 
 
 
