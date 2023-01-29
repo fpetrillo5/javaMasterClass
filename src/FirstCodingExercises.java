396998c9a44e24ec46903027ed27b448e716c082
Frank Petrillo <frankpetrillo5@gmail.com>
2023-01-29 10:32:22 -0500

    coding exercise 5.1

--- src/FirstCodingExercises.java
+++ src/FirstCodingExercises.java
@@ -13,6 +13,32 @@ public class FirstCodingExercises {
 //        printMegaBytesAndKiloBytes(5000);
 //        printMegaBytesAndKiloBytes(6500);
 
+
+
+        System.out.println(shouldWakeUp(true, 0));//false
+        System.out.println(shouldWakeUp(false, 0));//false
+        System.out.println(shouldWakeUp(true, -9));//false
+        System.out.println( shouldWakeUp(true, 1));//true
+        System.out.println(shouldWakeUp(false, 4));//false???
+        System.out.println(shouldWakeUp(true, 8));//false
+        System.out.println(shouldWakeUp(false, 8));//false
+        System.out.println(shouldWakeUp(true, 12));//false
+        System.out.println( shouldWakeUp(false, 20));//false
+        System.out.println( shouldWakeUp(true, 22));//false
+        System.out.println( shouldWakeUp(false, 22));//false
+        System.out.println( shouldWakeUp(true, 23));//true
+        System.out.println(shouldWakeUp(false, 23));//false
+        System.out.println( shouldWakeUp(true, 24));//false
+        System.out.println( shouldWakeUp(false, 24));//false
+        System.out.println( shouldWakeUp(true, 28));//false
+        System.out.println( shouldWakeUp(false, 254645));//false
+
+        System.out.println( shouldWakeUp(true, 1));
+        System.out.println( shouldWakeUp(false , 2));
+        System.out.println( shouldWakeUp(true, 8));
+        System.out.println( shouldWakeUp(true, -1));
+
+
     }
             public static long toMilesPerHour(double kilometersPerHour){
                 if(kilometersPerHour < 0){
@@ -40,7 +66,50 @@ public class FirstCodingExercises {
             System.out.println(kiloBytes +  " KB = " + kiloBytes/1024 + " MB and " + kiloBytes%1024 + " KB");
         }
             }
+
+            public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+
+        if(((hourOfDay >= 0 && hourOfDay < 8) && barking)|| ((hourOfDay > 22 && hourOfDay <24)) && barking)
+            return true;
+        else
+            return false;
+
+            }
     }
+
+    /*
+    Barking Dog
+
+We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
+
+Write a method shouldWakeUp that has 2 parameters.
+
+1st parameter should be of type boolean and be named barking it represents if our dog is currently barking.
+2nd parameter represents the hour of the day and is of type int with the name hourOfDay and has a valid range of 0-23.
+
+We have to wake up if the dog is barking before 8 or after 22 hours so in that case return true.
+
+In all other cases return false.
+
+If the hourOfDay parameter is less than 0 or greater than 23 return false.
+
+Examples of input/output:
+
+    shouldWakeUp (true, 1); → should return true
+
+    shouldWakeUp (false, 2); → should return false since the dog is not barking.
+
+    shouldWakeUp (true, 8); → should return false, since it's not before 8.
+
+    shouldWakeUp (true, -1); → should return false since the hourOfDay parameter needs to be in a range 0-23.
+
+
+TIP: Use the if else statement with multiple conditions.
+
+NOTE: The shouldWakeUp method  needs to be defined as public static ​like we have been doing so far in the course.
+
+NOTE: Do not add a  main method to solution code.
+     */
 
     /*
     MegaBytes Converter
