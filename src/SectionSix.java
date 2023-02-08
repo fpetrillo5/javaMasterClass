41729d872d164f56875f4a90941cb34c4bc5f2b3
frankpetrillo <frankpetrillo5@gmail.com>
2023-02-07 21:08:57 -0500

    coding exercise 19 - shared digit

--- src/SectionSix.java
+++ src/SectionSix.java
@@ -20,7 +20,8 @@ public class SectionSix {
         //class66Driver();
         //isPalindromeDriver();
         //sumFirstAndLastDigitDriver();
-        getEvenDigitSumDriver();
+        //getEvenDigitSumDriver();
+        hasSharedDigitDriver();
 
 
 
@@ -625,6 +626,40 @@ public static void class62Driver(){
         }while(number > 0);
 
         return sum;
+    }
+
+    public static void hasSharedDigitDriver(){
+        System.out.println(hasSharedDigit(12, 23));
+        System.out.println(hasSharedDigit(9, 99));
+        System.out.println(hasSharedDigit(15, 55));
+        System.out.println(hasSharedDigit(15, 555));
+        System.out.println(hasSharedDigit(77, 77));
+
+    }
+
+    public static boolean hasSharedDigit(int num1, int num2){
+        if((num1 <10 || num1 > 99) || (num2 < 10 || num2 > 99))
+            return false;
+
+        if(num1 == num2)
+            return true;
+
+        int glove1a = 0;
+        int glove1b = 0;
+        int glove2a = 0;
+        int glove2b = 0;
+
+        glove1a = num1 % 10;
+        num1 /= 10;
+        glove1b = num1 % 10;
+        glove2a = num2 % 10;
+        num2 /= 10;
+        glove2b = num2 % 10;
+        if((glove1a == glove2a) || glove1a == glove2b ||
+                glove1b == glove2a || glove1b == glove2b)
+            return true;
+        else
+            return false;
     }
 
 }
