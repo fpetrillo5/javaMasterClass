4a715014b521698baf9c6527c926c8562a956c1d
frankpetrillo <frankpetrillo5@gmail.com>
2023-02-16 12:30:01 -0500

    coding exercise 25 - flour pack problem

--- src/SectionSix.java
+++ src/SectionSix.java
@@ -27,7 +27,8 @@ public class SectionSix {
         //hasSameLastDigitDriver();
         //printFactorsDriver();
         //getGreatestCommonDivisorDriver();
-        class70Driver();
+        //class70Driver();
+        canPackDriver();
 
 
 
@@ -783,6 +784,48 @@ public static void class62Driver(){
         return  "so you are " + age + " years old";
     }
 
+    public static void canPackDriver(){
+        System.out.println(canPack(1,0,4)); //false
+        System.out.println(canPack(1,0,5));//true
+        System.out.println(canPack(0,5,4));//true
+        System.out.println(canPack(2,2,11));//true
+        System.out.println(canPack(-3,2,12));//false
+        System.out.println(canPack(3,2,22));//false
+        System.out.println(canPack(5,2,22));//true
+        System.out.println(canPack(30,2,22));//true
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if((bigCount < 0) || (smallCount < 0) || (goal < 0))
+            return false;
+
+        int realbigCount = bigCount * 5;
+
+        if(realbigCount + smallCount < goal){
+            return false;
+        }
+        else if(goal < 5){
+            if(smallCount >= goal)
+                return true;
+            else
+                return false;
+        }
+        else if(realbigCount < goal){
+            if(realbigCount + smallCount >= goal)
+                return true;
+            else
+                return false;
+        }
+        else if(realbigCount > goal){
+            int realgoalDiv5 = 5 * (goal / 5);
+            if(realgoalDiv5 + smallCount >=goal)
+                return true;
+            else
+                return false;
+        } else
+            return true;
+    }
 }
 
 //class 71- exception handling and intro to scanner
